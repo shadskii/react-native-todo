0f3d23fb475cd3dde4c16157a5c1e31ba3d30462
shadskii <jake.k.hassel@gmail.com>
2017-12-17 16:54:22 -0500

    Added actions

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+let nextToDoId = 0;
+
+export const addTodo = text => {
+    return {
+        type: 'ADD_TODO',
+        id: nextToDoId++,
+        text
+    }
+}
+
+export const setVisibilityFilter = filter => {
+    return {
+        type: 'SET_VISIBILITY_FILTER',
+        filter
+    }
+}
+
+export const toggleTodo = id => {
+    return {
+        type: 'TOGGLE_TODO',
+        id
+    }
+}
