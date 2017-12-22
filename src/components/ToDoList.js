cf65764aecc6b4f88d5a103562594ea430247344
shadskii <jake.k.hassel@gmail.com>
2017-12-21 22:58:32 -0500

    Added proptypes for todolist

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FlatList, View } from 'react-native';
 import { List, Container, Header, Content, ListItem, Text } from 'native-base';
 import ToDo from './ToDo';
@@ -10,5 +11,16 @@ export const ToDoList = ({ todos, onTodoClickToggle, onTodoClickDelete }) => (
             return <ToDo key={todo.id} {...todo} onClickToggle={() => onTodoClickToggle(todo.id)} onClickDelete={() => onTodoClickDelete(todo.id)} />
         })}
     </List>
-
 );
+
+ToDoList.propTypes = {
+    todos: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            completed: PropTypes.bool.isRequired,
+            text: PropTypes.string.isRequired
+        }).isRequired,
+    ).isRequired,
+    onTodoClickToggle: PropTypes.func.isRequired,
+    onTodoClickDelete: PropTypes.func.isRequired
+}
